f548801ec0317b30c298003629316b80e742004f
Lars Larsson <larsson.work@gmail.com>
2019-08-27 12:58:04 +0200

    Modern config location and remove DPMS

--- desktop/i3/screenlocking/lock.sh
+++ desktop/i3/screenlocking/lock.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ~/.i3/screenlocking
+cd ~/.config/i3/screenlocking
 
 IMAGE=lockscreen.png
 
 scrot $IMAGE
 #convert -blur 0x4 -type Grayscale lockscreen.png lockscreen.png
-convert $IMAGE -type Grayscale -level 0%,100%,0.6 -blur 0x5 -font Liberation-Sans -pointsize 48 -fill orange -gravity center -annotate +0+200 'Type password to unlock' - | composite -gravity center lock.png - $IMAGE
-i3lock --dpms --ignore-empty-password --image=lockscreen.png
+convert $IMAGE -type Grayscale -level 0%,100%,0.6 -blur 0x9 -font Liberation-Sans -pointsize 48 -fill orange -gravity center -annotate +0+200 'Type password to unlock' - | composite -gravity center lock.png - $IMAGE
+i3lock --ignore-empty-password --image=lockscreen.png
 rm $IMAGE
